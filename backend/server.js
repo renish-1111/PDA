0a4b54bd045beb5ae7fc28959b0abe9cf2d1847c
Renish Ponkiya <ponkiyarenish@gmail.com>
2024-02-23 21:59:03 +0530

    server file add

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,25 @@ app.use(express.json())
 app.use(cors())
 
 const db = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     user: "root",
     password: "",
     database: "signup"
 })
+
+app.post('/sign-up',(req,res)=>{
+    const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)"
+    const  values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql,[values],(err,data)=>{
+        if(err){
+            return res.json("Error")
+        }
+        return res.json(data)
+    })
+})
+
+app.listen(8081, () => { console.log("listening"); })
