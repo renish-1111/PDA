6c1d32f600e5683149184617122233f733525dcb
Renish Ponkiya <ponkiyarenish@gmail.com>
2024-02-27 23:20:11 +0530

    mongo

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,28 +1,74 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/pda")
+mongoose
+    .connect("mongodb://localhost:27017/pda")
     .then(() => {
         console.log("mongodb connected");
     })
     .catch(() => {
-        console.log('failed');
-    })
+        console.log("failed");
+    });
 
 const newSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true,
     },
     username: {
         type: String,
-        required: true
+        required: true,
     },
     password: {
         type: String,
-        required: true
-    }
-})
+        required: true,
+    },
+    q1: {
+        type: String,
+    },
+    q2: {
+        type: String,
+    },
+    q3: {
+        type: String,
+    },
+    q4: {
+        type: String,
+    },
+    q5: {
+        type: String,
+    },
+    q6: {
+        type: String,
+    },
+    q7: {
+        type: String,
+    },
+    q8: {
+        type: String,
+    },
+    q9: {
+        type: String,
+    },
+    q10: {
+        type: String,
+    },
+    q11: {
+        type: String,
+    },
+    q12: {
+        type: String,
+    },
+    q13: {
+        type: String,
+    },
+    q14: {
+        type: String,
+    },
+    q15: {
+        type: String,
+    },
+});
 
-const user = mongoose.model("user", newSchema)
+const user = mongoose.model("user", newSchema);
 
-module.exports = user
+module.exports = user;
