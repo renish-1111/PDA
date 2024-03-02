7caf4bd0fc703853a53d1ec65946f381e7edb7af
ShamikPatel25 <shamikpatoliya25@gmail.com>
2024-03-02 21:54:19 +0530

    add

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,8 +7,33 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      
-      
+      <div id="name">
+                  <span className="name">Name</span>{" "}
+                  <div>
+                    <input
+                      type="name"
+                      name="name"
+                      id="name"
+                      placeholder="Enter name"
+                      onChange={(e) => { setName(e.target.value) }}
+                      style={{ display: "block" }}
+                    />
+                  </div>
+        </div>
+        <div id="emial">
+                  <span className="email">Email</span>{" "}
+                  <div>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Enter Email"
+                      onChange={(e) => { setemail(e.target.value) }}
+                      style={{ display: "block" }}
+                    />
+                  </div>
+        </div>
+
     </div>
   );
 };
